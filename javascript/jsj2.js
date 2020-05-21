0283d6a2afb6221e43891e5a65f01980fd0fa14a
DencyDavid <dencydavid1997@gmail.com>
2020-05-21 11:00:53 +0530

    second command

---
+++ javascript/jsj2.js
@@ -0,0 +1,8 @@
+// this is a single line comment
+var num1=Number(prompt("Num1: "));
+var num2=Number(prompt("Num2: "));
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1/num2);
+console.log(num1*num2);
+console.log(num1%num2);
